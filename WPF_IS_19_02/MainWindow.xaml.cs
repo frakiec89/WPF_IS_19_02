fc66a433c1de5dbaea8908de91280e582cf8d886
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-09-07 16:46:46 +0400

    Добавил  авторизацию

--- WPF_IS_19_02/MainWindow.xaml.cs
+++ WPF_IS_19_02/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace WPF_IS_19_02
 {
     /// <summary>
@@ -24,9 +12,6 @@ namespace WPF_IS_19_02
         {
             InitializeComponent();
         }
-
-       
-       
         private void btGo_Click(object sender, RoutedEventArgs e)
         {
             try
