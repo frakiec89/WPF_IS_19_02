19b5e707aea5b18fc7b2ee9c2dd3026bc5f05341
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-09-09 09:36:29 +0400

    Пишу  димамичную верстку

--- WPF_IS_19_02/View/WindowMaterilSklad .xaml.cs
+++ WPF_IS_19_02/View/WindowMaterilSklad .xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPF_IS_19_02.View.ModelView;
 
 namespace WPF_IS_19_02.View
 {
@@ -19,17 +20,32 @@ namespace WPF_IS_19_02.View
     /// </summary>
     public partial class WindowMaterilSklad : Window
     {
+        private List<View.ModelView.ViewMaterial> content = new List<ModelView.ViewMaterial>();
+
         public WindowMaterilSklad()
         {
             InitializeComponent();
 
             try
             {
-               lbContent.ItemsSource = ControllerMaterial.GetViewMaterial();
+               content = GetContent();
+                lbContent.ItemsSource = content;
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private List<ViewMaterial> GetContent()
+        {
+            try
+            {
+                return ControllerMaterial.GetViewMaterial();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
             }
         }
 
@@ -43,6 +59,41 @@ namespace WPF_IS_19_02.View
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             MessageBox.Show("Объект не  найден");
+        }
+
+
+        private void DinamicStakBytton( int count )
+        {
+            int countButton = WindosMaterialService.GetCountButton(count); 
+            
+
+
+
+        }
+
+
+        private Button CreateButtun (string name , string content,  RoutedEventHandler  action )
+        {
+           var b = new Button() { Name = name, Content = content };
+            b.Click+=action ;
+            return b;
+        }
+
+        
+    }
+
+    public class WindosMaterialService
+    {
+        public static int GetCountButton(int count)
+        {
+            if (count % 15 == 0)
+            {
+                return count / 15;
+            }
+            else
+            {
+                return count / 15 + 1;
+            }
         }
     }
 }
---
+++ WPF_IS_19_02Tests/WindowMaterilSkladTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPF_IS_19_02.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_IS_19_02.View.Tests
+{
+    [TestClass()]
+    public class WindowMaterilSkladTests 
+    {
+        [TestMethod()]
+        public void GetCountButtonTest_30_2()
+        {
+            int count = 30;
+            int expected = 2;
+
+            var actual = WindosMaterialService.GetCountButton(count);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetCountButtonTest_31_3()
+        {
+            int count = 31;
+            int expected = 3;
+            var actual = WindosMaterialService.GetCountButton(count);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetCountButtonTest_5_1()
+        {
+            int count = 5;
+            int expected = 1;
+            var actual = WindosMaterialService.GetCountButton(count);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetCountButtonTest_60_4()
+        {
+            int count = 60;
+            int expected = 4;
+            var actual = WindosMaterialService.GetCountButton(count);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
