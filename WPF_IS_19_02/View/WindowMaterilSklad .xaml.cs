6373f34d5713db5b3ee90e622a3b59aa3a3c0900
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-09-09 10:48:51 +0400

    Добавил Листы

--- WPF_IS_19_02/View/WindowMaterilSklad .xaml.cs
+++ WPF_IS_19_02/View/WindowMaterilSklad .xaml.cs
@@ -20,6 +20,9 @@ namespace WPF_IS_19_02.View
     /// </summary>
     public partial class WindowMaterilSklad : Window
     {
+        private int actualList = 1;
+        private int actualMax;
+
         private List<View.ModelView.ViewMaterial> content = new List<ModelView.ViewMaterial>();
 
         public WindowMaterilSklad()
@@ -28,8 +31,14 @@ namespace WPF_IS_19_02.View
 
             try
             {
-               content = GetContent();
+                content = GetContent();
                 lbContent.ItemsSource = content;
+                DinamicStakBytton(content.Count);
+                actualMax = spButtons.Children.Count - 2;
+                var s = WindosMaterialService.IntMin(actualList);
+                RefreshContent(s, WindosMaterialService.CountContent(s, content.Count));
+                labelList.Content = $"лист {actualList}";
+
             }
             catch(Exception ex)
             {
@@ -62,11 +71,51 @@ namespace WPF_IS_19_02.View
         }
 
 
-        private void DinamicStakBytton( int count )
+        private void  DinamicStakBytton( int count )
         {
-            int countButton = WindosMaterialService.GetCountButton(count); 
+            int countButton = WindosMaterialService.GetCountButton(count);
+            spButtons.Children.Add(CreateButtun("btDown", "<<", btDown_Ckick)); // Добавил первую кнопку 
             
+            for (int i = 0; i < WindosMaterialService.GetCountButton(count); i++) // генерация кнопок
+            {
+                spButtons.Children.Add(CreateButtun($"btNext{i}",  $"{i+1}", btNext_Ckick)); // Добавил первую кнопку 
+            }
+            spButtons.Children.Add(CreateButtun("btUp", ">>", btUp_Ckick)); // Добавил первую кнопку 
+        }
 
+        private void btDown_Ckick ( object Sender , RoutedEventArgs e)
+        {
+            if(actualList>1)
+            {
+                actualList--;
+                var s =  WindosMaterialService.IntMin(actualList);
+                RefreshContent(s, WindosMaterialService.CountContent(s, content.Count));
+            }
+        }
+
+        private void RefreshContent(int start, int  end)
+        {
+            var s = content.GetRange(start, end);
+            lbContent.ItemsSource = s;
+            labelList.Content = $"лист {actualList}";
+        }
+
+        private void btUp_Ckick(object Sender, RoutedEventArgs e)
+        {
+            if (actualList <actualMax  )
+            {
+                actualList++;
+                var s = WindosMaterialService.IntMin(actualList);
+                RefreshContent(s, WindosMaterialService.CountContent(s,content.Count));
+            }
+        }
+
+        private void btNext_Ckick(object Sender, RoutedEventArgs e)
+        {
+            var but = e.OriginalSource as Button;
+            actualList = Convert.ToInt32(but.Content.ToString());
+            var s = WindosMaterialService.IntMin(actualList);
+            RefreshContent(s, WindosMaterialService.CountContent(s, content.Count));
 
 
         }
@@ -74,12 +123,14 @@ namespace WPF_IS_19_02.View
 
         private Button CreateButtun (string name , string content,  RoutedEventHandler  action )
         {
-           var b = new Button() { Name = name, Content = content };
+           var b = new Button() { Name = name, Content = content , Margin= new Thickness(5) };
+            b.Background = new SolidColorBrush(Color.FromArgb(255, 255, 193, 193));
+            b.HorizontalAlignment = HorizontalAlignment.Center;
             b.Click+=action ;
             return b;
         }
 
-        
+
     }
 
     public class WindosMaterialService
@@ -95,5 +146,23 @@ namespace WPF_IS_19_02.View
                 return count / 15 + 1;
             }
         }
+
+        public static int  IntMin (int list)
+        {
+            return (list * 15)  -15;
+        }
+
+        public static int CountContent (int  start , int Maxcount)
+        {
+            int rez = Maxcount - start;
+
+            if (rez >= 15)
+                return 15;
+            else
+            {
+              return  rez;
+            }
+        }
+
     }
 }
---
+++ WPF_IS_19_02Tests/WindosMaterialServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPF_IS_19_02.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_IS_19_02.View.Tests
+{
+    [TestClass()]
+    public class WindosMaterialServiceTests
+    {
+        [TestMethod()]
+        public void intMinTest_2_15()
+        {
+            int list = 2;
+
+            int expected = 15;
+
+            var actual = WindosMaterialService.IntMin(list);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountContentTest_15_31_15()
+        {
+            int list = 15;
+            int Count = 31;
+            int expected = 15;
+            var actual = WindosMaterialService.CountContent(list , Count);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountContentTest_30_31_1()
+        {
+            int list = 30;
+            int Count = 31;
+            int expected = 1;
+            var actual = WindosMaterialService.CountContent(list, Count);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void CountContentTest_0_14_14()
+        {
+            int list = 0;
+            int Count = 14;
+            int expected = 14;
+            var actual = WindosMaterialService.CountContent(list, Count);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
